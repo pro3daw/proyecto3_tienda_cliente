baba73649964beaa2570958a3a0d0b25a0393a0c
alumno <alumno@2A5PC20>
2014-02-12 12:02:42 +0100

    Commit cristian carrito inserta correctamente cabecera y lineas

--- compra.php
+++ compra.php
@@ -17,4 +17,25 @@ $sql = "INSERT INTO pedidos (id_usuario, estado_envio, precio_pedido, fecha_pedi
 
 mysql_query($sql) or die ("Couldn t execute query.".mysql_error());
 
+$sql = "SELECT MAX(id_pedido) as maximo from pedidos where id_usuario=$cliente;";
+
+$result =mysql_query($sql) or die ("Couldn t execute query.".mysql_error());
+
+$fila= mysql_fetch_array($result,MYSQL_ASSOC);
+//$datos=array('id'=>$fila["maximo"]);
+//echo $datos['id'];
+$idPedido = $fila["maximo"];
+
+
+
+foreach ($carrito->listaProductos as $producto) {
+    
+    $sql = "INSERT INTO detalle_pedidos (id_pedido, id_producto, unidades, precio_unidad,total_linea) VALUES ($idPedido,$producto->id,$producto->unidades,$producto->precio,$producto->total);";
+
+    mysql_query($sql) or die ("Couldn t execute query.".mysql_error());
+    
+}
+
+
+
 ?>
